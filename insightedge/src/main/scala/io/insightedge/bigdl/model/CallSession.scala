884e43f610d7b4efe020fc2080436b766ccb3fef
Danylo Hurin <danylo@gigaspaces.com>
2017-08-14 17:17:48 +0300

    Add CallSession model

---
+++ insightedge/src/main/scala/io/insightedge/bigdl/model/CallSession.scala
@@ -0,0 +1,32 @@
+//   scalastyle:off
+
+package io.insightedge.bigdl.model
+
+import org.insightedge.scala.annotation.SpaceId
+//import org.openspaces.textsearch.SpaceTextIndex
+
+import scala.beans.BeanProperty
+
+/**
+  * @author Danylo_Hurin.
+  */
+case class CallSession(
+                 @BeanProperty
+                 @SpaceId(autoGenerate = false) // same as Prediction/
+                 var id: String,
+
+                 @BeanProperty
+//                 @SpaceTextIndex
+                 var text: String,
+
+                 @BeanProperty
+                 var category: String,
+
+                 @BeanProperty // dummy data
+                 var agentId: String
+) {
+  def this() = this(null, null, null, null)
+
+}
+
+
