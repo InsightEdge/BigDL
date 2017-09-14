51e18c4f17e75c4e0e4a905d4c3eba564bd630b1
Danylo Hurin <danylo@gigaspaces.com>
2017-09-14 19:11:22 +0300

    Update RegressionJob

--- insightedge/runRegressionJob.sh
+++ insightedge/runRegressionJob.sh
@@ -7,4 +7,4 @@ $INSIGHTEDGE_HOME/bin/insightedge-submit --master spark://127.0.0.1:7077 \
            --driver-memory 2g --executor-memory 2g  \
            --total-executor-cores 2 --executor-cores 2 \
            --class io.insightedge.bigdl.LinearRegressionJob \
-           ./target/insightedge-0.2.0-jar-with-dependencies.jar
+           ./spark/target/spark-0.2.0-jar-with-dependencies.jar
--- insightedge/spark/src/main/scala/io/insightedge/bigdl/LinearRegressionJob.scala
+++ insightedge/spark/src/main/scala/io/insightedge/bigdl/LinearRegressionJob.scala
@@ -9,6 +9,7 @@ import com.intel.analytics.bigdl.optim.{Optimizer, Trigger, _}
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.utils.Engine
 import org.apache.spark.SparkContext
+import org.apache.spark.mllib.linalg
 import org.apache.spark.rdd.RDD
 import org.insightedge.spark.context.InsightEdgeConfig
 import org.insightedge.spark.implicits.basic._
@@ -19,11 +20,10 @@ import scala.language.existentials
 
 object LinearRegressionJob {
 
-
-  val FEATURES_DIM = 2
-  val data_len = 100
+  val FEATURES_DIM = 1
+  val data_len = 1000
   val learning_rate = 0.2
-  val training_epochs = 5
+  val training_epochs = 1
   val batch_size = 4
   val n_input = FEATURES_DIM
   val n_output = 1
@@ -37,8 +37,10 @@ object LinearRegressionJob {
 
   // TODO not sure about correctness of data generation
   def generateRandomSample(): Sample[(Float)] = {
-    val features: Array[Float] = Array(r.nextFloat(), r.nextFloat())
-    val label: Float = features.sum * 2f + 0.4f
+//    x -> input
+//    y -> output
+    val features: Array[Float] = Array(r.nextFloat()) // x
+    val label: Float = features.sum * 2f + 0.4f // y
     val tensor: Tensor[Float] = Tensor(features, Array(FEATURES_DIM))
     val sample: Sample[Float] = Sample(tensor, label)
     sample
@@ -53,12 +55,13 @@ object LinearRegressionJob {
   def main(args: Array[String]): Unit = {
     val ieConfig = InsightEdgeConfig("insightedge-space", Some("insightedge"), Some("127.0.0.1:4174"))
     val conf = Engine.createSparkConf()
-      .setAppName("Text classification")
+      .setAppName("Linear Regression")
       .set("spark.task.maxFailures", "1").setInsightEdgeConfig(ieConfig)
     val sc = SparkContext.getOrCreate(conf)
     Engine.init
 
-    val sampleRDD: RDD[Sample[Float]] = sc.parallelize(0 to 1000).map(_ => generateRandomSample())
+    val sample = (0 to data_len).map(_ => generateRandomSample()).toList
+    val sampleRDD: RDD[Sample[Float]] = sc.parallelize(sample)
     val Array(trainingRDD, valRDD) = sampleRDD.randomSplit(
       Array(trainingSplit, 1 - trainingSplit))
 
@@ -78,8 +81,21 @@ object LinearRegressionJob {
 
     prediction.take(10).foreach { p =>
       println("Prediction: " + p)
-    }
+      if (p.isTable) {
+        println("Should not be here")
+      } else {
+        val tensor = p.toTensor[Float]
+        val sizes: Array[Int] = tensnor.size()
+        println("Tensor size: " + sizes.deep.mkString(", "))
+        println("Tensor: " + tensor)
+//        val vector: linalg.Vector = tensor.toMLlibVector()
+//        println("List: " + vector.toArray.toList)
 
+        println("Value at 0: " + tensor(0))
+        println("Value at 0: " + tensor.valueAt(0))
+        println("Value at 1: " + tensor.valueAt(1))
+      }
+    }
     valRDD.take(10).toList.foreach {
       i =>
       println("data: " + i.getData().deep.mkString(", "))
