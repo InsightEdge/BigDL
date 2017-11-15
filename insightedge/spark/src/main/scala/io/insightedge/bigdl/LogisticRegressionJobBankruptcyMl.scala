00625115802170f3074b1e3ddd2baef862ba8552
Danylo Hurin <danylo@gigaspaces.com>
2017-11-15 11:31:53 +0200

    Cleanup logistic regression job

--- insightedge/spark/src/main/scala/io/insightedge/bigdl/LogisticRegressionJobBankruptcyMl.scala
+++ insightedge/spark/src/main/scala/io/insightedge/bigdl/LogisticRegressionJobBankruptcyMl.scala
@@ -2,7 +2,6 @@
 package io.insightedge.bigdl
 
 import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionSummary}
-import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.rdd.RDD
@@ -11,10 +10,6 @@ import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructTy
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.udf
 
-import scala.language.existentials
-
-
-// $example on$
 
 object LogisticRegressionJobBankruptcyMl {
 
@@ -27,13 +22,9 @@ object LogisticRegressionJobBankruptcyMl {
       .appName("LogisticRegressionBankruptcyMl")
       .getOrCreate()
 
-    val data: RDD[String] = spark.sparkContext.textFile("/home/dgurin/Downloads/Qualitative_Bankruptcy/Qualitative_Bankruptcy/Qualitative_Bankruptcy.data.txt")
-    println(data.count())
+    val rawData: RDD[String] = spark.sparkContext.textFile("/home/dgurin/Downloads/Qualitative_Bankruptcy/Qualitative_Bankruptcy/Qualitative_Bankruptcy.data.txt")
+    println(rawData.count())
 
-    val schemaString = "label features_raw"
-
-    val fields = schemaString.split(" ")
-      .map(fieldName => StructField(fieldName, DoubleType))
     val schema = StructType(Array(StructField("label", DoubleType), StructField("features_raw", StringType)))
 
     val toUdfVector: UserDefinedFunction = udf[Vector, String] { x =>
@@ -42,36 +33,25 @@ object LogisticRegressionJobBankruptcyMl {
       Vectors.dense(features.map(s => getDoubleValue(s)))
     }
 
-    val parsedData: RDD[Row] = data.map { line =>
+    val rawRddData: RDD[Row] = rawData.map { line =>
       val parts = line.split(",")
       Row(getDoubleValue(parts(6)), line)
     }
 
-    val df = spark.createDataFrame(parsedData, schema)
-    df.printSchema()
+    val df = spark.createDataFrame(rawRddData, schema)
 
-    val df2 = df.withColumn("features_raw", toUdfVector(df("features_raw")))
-    val assembler = new VectorAssembler().
-      setInputCols(Array("features_raw")).
-      setOutputCol("features")
+    val inputData = df.withColumn("features", toUdfVector(df("features_raw")))
+    inputData.printSchema()
 
-    val out = assembler.transform(df2)
-    out.printSchema()
-
-    val splits = out.randomSplit(Array(0.6, 0.4), seed = 11L)
+    val splits = inputData.randomSplit(Array(0.6, 0.4), seed = 11L)
     val trainingData = splits(0)
     val testData = splits(1)
-
-    trainingData.printSchema()
 
     val model = new LogisticRegression().fit(trainingData)
 
     val results: LogisticRegressionSummary = model.evaluate(testData)
     results.predictions.createOrReplaceTempView("predictions")
     println(results.predictions.show(20))
-    println("labels " + results.labelCol)
-    println("features " + results.featuresCol)
-    println("Propbs " + results.probabilityCol)
     spark.stop()
   }
 
